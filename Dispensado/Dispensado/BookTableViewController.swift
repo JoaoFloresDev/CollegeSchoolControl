510b7b754e01f861bf3e3dc75eccd934681bf854
Joao Victor Flores <j199818@dac.unicamp.br>
2020-10-07 16:43:37 -0300

    up

--- Dispensado/Dispensado/BookTableViewController.swift
+++ Dispensado/Dispensado/BookTableViewController.swift
@@ -71,8 +71,6 @@ class BookTableViewController: UITableViewController {
         return cell
     }
     
-
-    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
